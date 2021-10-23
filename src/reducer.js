b450d1373ada1c095b8cb71bdf4d92ce296f2d49
gnoul_ <ttluong4869@gmail.com>
2021-10-23 12:02:13 +0700

    Complete the project

---
+++ src/reducer.js
@@ -0,0 +1,72 @@
+const reducer = (state, action) => {
+    if (action.type === 'CLEAR_CART') {
+        return { ...state, cart: [] };
+    }
+    if (action.type === 'REMOVE') {
+        const newCart = state.cart.filter((item) => item.id !== action.payload);
+        return { ...state, cart: newCart };
+    }
+    // if (action.type === 'INCREASE') {
+    //     let tempCart = state.cart.map((item) => {
+    //         if (item.id === action.payload) {
+    //             return { ...item, amount: item.amount + 1 };
+    //         }
+    //         return item;
+    //     });
+    //     return { ...state, cart: tempCart };
+    // }
+    // if (action.type === 'DECREASE') {
+    //     let tempCart = state.cart
+    //         .map((item) => {
+    //             if (item.id === action.payload) {
+    //                 return { ...item, amount: item.amount - 1 };
+    //             }
+    //             return item;
+    //         })
+    //         .filter((item) => item.amount !== 0);
+    //     return { ...state, cart: tempCart };
+    // }
+    if (action.type === 'TOGGLE_AMOUNT') {
+        let tempCart = state.cart
+            .map((item) => {
+                if (item.id === action.payload.id) {
+                    if (action.payload.type === 'increase') {
+                        return { ...item, amount: item.amount + 1 };
+                    }
+                    if (action.payload.type === 'decrease') {
+                        return { ...item, amount: item.amount - 1 };
+                    }
+                }
+                return item;
+            })
+            .filter((item) => item.amount !== 0);
+        return { ...state, cart: tempCart };
+    }
+    if (action.type === 'GET_TOTALS') {
+        let { total, amount } = state.cart.reduce(
+            (cartTotal, cartItem) => {
+                const { price, amount } = cartItem;
+                const itemTotal = price * amount;
+
+                cartTotal.amount += amount;
+                cartTotal.total += itemTotal;
+                return cartTotal;
+            },
+            {
+                total: 0,
+                amount: 0,
+            }
+        );
+        total = parseFloat(total.toFixed(2));
+        return { ...state, total, amount };
+    }
+    if (action.type === 'LOADING') {
+        return { ...state, loading: true };
+    }
+    if (action.type === 'DISPLAY_ITEMS') {
+        return { ...state, cart: action.payload, loading: false };
+    }
+    throw new Error('No matching action type');
+};
+
+export default reducer;
